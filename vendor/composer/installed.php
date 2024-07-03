7645650c582ee48c41c7bfb8c8c3349ae2ed70c1
Gabriel Asturias <gabrielasturias@outlook.com>
2024-07-03 13:36:23 +0800

    refresh lang

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter4/appstarter',
-        'pretty_version' => 'v4.5.1',
-        'version' => '4.5.1.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fc0b37faf249ee4465515423ec90d9c40285890a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codeigniter4/appstarter' => array(
-            'pretty_version' => 'v4.5.1',
-            'version' => '4.5.1.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fc0b37faf249ee4465515423ec90d9c40285890a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
